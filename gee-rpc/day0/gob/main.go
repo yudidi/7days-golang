966495d2f29513744a7c01dd067b646cb25d7d6b
yudidi <972656027@qq.com>
2021-03-15 10:20:53 +0800

    预备知识gob,bufio。 Q: 为什么需要把数据结构进行编码？

--- gee-rpc/day0/gob/main.go
+++ gee-rpc/day0/gob/main.go
@@ -11,6 +11,11 @@ type MsgData struct {
 	Name    string
 }
 
+type MsgData2 struct {
+	X, Y, Z int
+	Name    string
+}
+
 var network bytes.Buffer //网络传递的数据载体
 // 对编码器,where to send the data,编码器把数据写到的这里去
 // 对解码器.source of the data,从这里读取待解码的数据
@@ -27,20 +32,26 @@ func main() {
 	}
 }
 
+// sendMsg -> network -> revData
+// sendMsg -> network -> revData
+// src1 -> desc1, src2 -> desc2 // TODO 解码和编码器需要desc1和src2是不变的,所以作为构造函数的参数。 而src1和desc2是变化的
+// TODO 进一步可对照物理世界:
+//  1个密码编码器创建时，存储模块(能够写入东西的模块Write)就一起创建了，但是需要编码的东西则还不确定(所以是interface{})
+//  1个解码器创建时,读取模块(能够读取密文的模块Read)就一起创建了,但是解码的结果还不确定(所以是interface{})
 func senMsg() error {
 	fmt.Print("开始执行编码（发送端）")
-
-	enc := gob.NewEncoder(&network)
+	enc := gob.NewEncoder(&network) // 数据编码后写入network // 编码器需要1个存放编码结果的地方.
 	sendMsg := MsgData{3, 4, 5, "jiangzhou"}
 	fmt.Printf("原始数据：%+v \n", sendMsg)
 	err := enc.Encode(&sendMsg)
 	fmt.Println("传递的编码数据为：", network) // ydd: 编码后写到了这里,可看到编码结果
 	return err
 }
+
 func revMsg() error {
 	var revData MsgData
-	dec := gob.NewDecoder(&network)
-	err := dec.Decode(&revData) //传递参数必须为 地址
+	dec := gob.NewDecoder(&network) // 从network读取数据,然后解码 // 解码器需要1个读取数据的东西
+	err := dec.Decode(&revData)     //传递参数必须为 地址
 	fmt.Printf("解码之后的数据为：%+v \n", revData)
 	return err
 }
--- gee-rpc/day0/readme.md
+++ gee-rpc/day0/readme.md
@@ -1,3 +1,14 @@
+# [gob解码编码流程 和 核心元素](https://www.processon.com/view/link/6047349207912947636e32a9)
+// sendMsg -> network -> revData
+// src1 -> desc1, src2 -> desc2
+```
+enc := gob.NewEncoder(&network) // desc1 // 构造函数
+err := enc.Encode(&sendMsg) // src1
+
+dec := gob.NewDecoder(&network)  // src2
+err := dec.Decode(&revData) // desc2
+```
+
 [Golang Gob编码（gob包的使用）](https://blog.csdn.net/weixin_42117918/article/details/105864520)
 
 # Q: gob编码的作用
@@ -9,4 +20,6 @@ A: 把xxx编码为bytes,字节流,这样可以在网络上传输
 > [gob编码,详解](http://c.biancheng.net/view/4597.html)
 
 # Q: 为什么需要把数据结构进行编码？
-https://www.kdocs.cn/l/seuYeLKiXaus?f=130
+有个小问题想请教下。 就是为什么序列化协议要搞这么多呢？ 从结构体变为字节流，固定一种方式不就好了吗。
+[wps:自己动手写rpc](https://www.kdocs.cn/l/seuYeLKiXaus?f=130)
+[](https://www.cnblogs.com/yudidi/p/14507632.html)
