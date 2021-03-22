64ac584e15e5a3066df77d7cc9a6a6b1d3079cf8
yudidi <972656027@qq.com>
2021-03-22 19:12:22 +0800

    stash

---
+++ gee-rpc/day0-pb-json编码效率-TODO/main.go
@@ -0,0 +1,6 @@
+package main
+
+// TODO pb编码出来的字节流更短(https://time.geekbang.org/column/article/247544)
+func main() {
+
+}
--- gee-rpc/day1-自己设计/main/main.go
+++ gee-rpc/day1-自己设计/main/main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
 	"net"
+	"os"
+)
+
+const (
+	CONN_HOST = "localhost"
+	CONN_PORT = "3333"
+	CONN_TYPE = "tcp"
 )
 
 func main() {
-
+	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	defer l.Close()
+	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		coder := Coder{conn: conn}
+		//coder.Write(&Data{ii: 1})
+		fmt.Println("coder read ", coder.Read())
+	}
 }
 
 // 实现从网络连接中读写数据
@@ -28,17 +53,34 @@ func (coder *Coder) Write(goData interface{}) {
 
 func (c *Coder) encode(sendData interface{}) []byte {
 	// TODO
-	return nil
+	var storeEncodeResult bytes.Buffer
+	enc := gob.NewEncoder(&storeEncodeResult) // // 指定存储编码结果的模块
+	err := enc.Encode(&sendData)              // 数据编码后写入storeEncodeResult
+	if err != nil {
+		fmt.Println("编码失败", err)
+	}
+	return storeEncodeResult.Bytes()
 }
 
-func (c *Coder) decode(data []byte) interface{} {
+type Data struct {
+	ii int
+}
+
+func (c *Coder) decode(data []byte) Data {
 	// TODO
-	return nil
+	var storeEncodeResult bytes.Buffer
+	dec := gob.NewDecoder(&storeEncodeResult) // 指定读取模块,读取待编码数据
+	d := Data{}
+	err := dec.Decode(&d) //传递参数必须为 地址
+	if err != nil {
+		fmt.Println(err)
+	}
+	return d
 }
 
 // 真正读取数据
 func connRead(conn net.Conn) []byte {
-	buf := make([]byte, 1024)     // Make a buffer to hold incoming data.
+	buf := make([]byte, 1024)     // Make a buffer to hold incoming Data.
 	reqLen, err := conn.Read(buf) // Read the incoming connection into the buffer.
 	fmt.Println("server received", reqLen, string(buf), err)
 	return buf
