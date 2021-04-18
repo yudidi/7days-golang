bfb1ad57f424b08eb047ad23f7e810bc5a754303
yudidi <972656027@qq.com>
2021-04-18 21:04:55 +0800

    add comment

---
+++ gee-rpc/day0-codec-粘包拆包/scanner/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// An artificial input source.
+	const input = "1234 5678 1234567901234567890"
+	scanner := bufio.NewScanner(strings.NewReader(input))
+
+	// Create a custom split function by wrapping the existing ScanWords function.
+	split := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+		fmt.Println("data:", data, string(data))
+		advance, token, err = bufio.ScanWords(data, atEOF)
+		fmt.Println("advance:", advance, "token:", token, string(token))
+		if err == nil && token != nil {
+			_, err = strconv.ParseInt(string(token), 10, 32)
+		}
+		return
+	}
+
+	// Set the split function for the scanning operation.
+	scanner.Split(split)
+	// Validate the input
+	for scanner.Scan() {
+		fmt.Printf("%s\n", scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Invalid input: %s", err)
+	}
+}
